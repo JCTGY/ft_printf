e4b955dab5aab454204570d30796d5297d5c0bb6
Jeffrey  Chiang <jchiang-@e1z2r3p30.42.us.org>
2019-03-02 09:52:06 -0800

    add author & add uint in output

--- src/pr_output.c
+++ src/pr_output.c
@@ -6,7 +6,7 @@
 /*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 09:11:41 by jchiang-          #+#    #+#             */
-/*   Updated: 2019/03/01 14:13:32 by jchiang-         ###   ########.fr       */
+/*   Updated: 2019/03/02 09:48:34 by jchiang-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int				pr_output(t_printf *p)
 	(p->con & CN_C || p->con & CN_M) && printf_character(p);
 	(p->con & CN_S) && printf_string(p);
 	(p->con & CN_D || p->con & CN_I) && printf_integer(p);
+	(p->con & CN_U) && printf_unsigned_int(p);
 	return (1);
 }
